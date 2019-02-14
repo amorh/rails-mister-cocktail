0b75e90aa9e744e6e2b0ab5dab475561a8cec95f
Hamza Amor <hamza.amor93@gmail.com>
2019-02-14 12:29:10 +0100

    commit ex 2

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  validates :description, presence: :true
+  validates :ingredient, presence: :true
+  validates :cocktail, presence: :true, uniqueness: { scope: :ingredient }
+  belongs_to :cocktail
+  belongs_to :ingredient
+end
